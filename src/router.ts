6a32ca9565ef59d24a358e1bf2b9d67308fbece5
PixelSam123 <77851256+PixelSam123@users.noreply.github.com>
2024-03-30 14:16:31 +0700

    remove unneccesary variable in resolving route handler

--- src/router.ts
+++ src/router.ts
@@ -19,25 +19,21 @@ export class Router {
     const pathname = new URL(req.url).pathname
 
     let handler: RouteHandler | undefined
-    let trailingPathIndex = pathname.length
-    {
-      let partialPathname = pathname
+    let partialPathname = pathname
 
-      for (
-        let lastSlashIndex = partialPathname.lastIndexOf('/');
-        lastSlashIndex !== -1;
-        lastSlashIndex = partialPathname.lastIndexOf('/')
-      ) {
-        const handlerToAssign = this.pathnameToHandler[partialPathname]
+    for (
+      let lastSlashIndex = partialPathname.lastIndexOf('/');
+      lastSlashIndex !== -1;
+      lastSlashIndex = partialPathname.lastIndexOf('/')
+    ) {
+      const handlerToAssign = this.pathnameToHandler[partialPathname]
 
-        if (typeof handlerToAssign !== 'undefined') {
-          handler = handlerToAssign
-          break
-        }
-
-        partialPathname = partialPathname.slice(0, lastSlashIndex)
-        trailingPathIndex = lastSlashIndex
+      if (typeof handlerToAssign !== 'undefined') {
+        handler = handlerToAssign
+        break
       }
+
+      partialPathname = partialPathname.slice(0, lastSlashIndex)
     }
 
     if (typeof handler === 'undefined') {
@@ -47,7 +43,7 @@ export class Router {
     return handler({
       req,
       server,
-      trailingPath: pathname.slice(trailingPathIndex),
+      trailingPath: pathname.slice(partialPathname.length),
     })
   }
 }
